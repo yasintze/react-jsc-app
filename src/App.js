2263fdb31afd9fb9ea8ad7fac4e0b065a6f55068
yasin <yasintze@gmail.com>
2019-11-12 11:13:36 +0700

    Add yup and configure basic react hooks form

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
 // @flow
 
 import React from "react";
+import useForm from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = data => {
+    console.log("submit", data);
+  };
+
+  console.log(watch("username"));
+
   return (
     <div className="App">
       <div className="w-full max-w-md bg-gray-800">
-        <form action="" className=" bg-white shadow-md rounded px-8 py-8 pt-8">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className=" bg-white shadow-md rounded px-8 py-8 pt-8"
+        >
           <div className="px-4 pb-4">
-            <label htmlFor="email" className="text-sm block font-bold  pb-2">
-              EMAIL ADDRESS
+            <label htmlFor="text" className="text-sm block font-bold  pb-2">
+              Username
             </label>
             <input
-              type="email"
-              name="email"
-              id=""
+              type="text"
+              name="username"
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 "
               placeholder="Johnbull@example.com"
+              ref={register({ required: true })}
             />
           </div>
           <div className="px-4 pb-4">
             <label htmlFor="password" className="text-sm block font-bold pb-2">
-              PASSWORD
+              Password
             </label>
             <input
               type="password"
-              name="email"
-              id=""
+              name="password"
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300"
               placeholder="Enter your password"
+              ref={register({ required: true })}
             />
           </div>
           <div>
+            {errors.exampleRequired && <span>This field is required</span>}
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
+              type="submit"
             >
               Sign In
             </button>
